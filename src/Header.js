5334d60a1460a0609f4db185db0069f406108c26
NazimMv <nazim.mukhtarbekov26@gmail.com>
2023-10-28 21:31:35 +0600

    added react app

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+      <header>
+        <nav className="navbar navbar-light bg-light">
+          <span className="navbar-brand">
+            Todo list
+          </span>
+        </nav>
+      </header>);
+  }
