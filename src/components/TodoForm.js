e1cb6a2a89227389b2ef81e33bb99cbe03583b6d
NazimMv <nazim.mukhtarbekov26@gmail.com>
2023-11-02 02:57:44 +0600

    added update logic

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,21 +2,14 @@ import React, { useState, useRef, useEffect } from 'react';
 
 function TodoForm({ onSubmit, edit }) {
   const [input, setInput] = useState(edit ? edit.text : '');
-  const [description, setDescription] = useState(edit ? edit.description : '');
-
   const inputRef = useRef(null);
 
   useEffect(() => {
     inputRef.current.focus();
-  });
+  }, []);
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    if (name === 'text') {
-      setInput(value);
-    } else if (name === 'description') {
-      setDescription(value);
-    }
+    setInput(e.target.value);
   };
 
   const handleSubmit = (e) => {
@@ -25,35 +18,21 @@ function TodoForm({ onSubmit, edit }) {
     onSubmit({
       id: edit ? edit.id : Math.floor(Math.random() * 10000),
       text: input,
-      description: description,
     });
 
     setInput('');
-    setDescription('');
   };
 
   return (
     <form onSubmit={handleSubmit} className='todo-form'>
-      {/* Todo TextArea */}
       <textarea
-        placeholder='Add a todo'
+        placeholder={edit ? 'Update your todo' : 'Add a todo'}
         value={input}
         onChange={handleChange}
-        name='text'
         className='todo-textarea'
         ref={inputRef}
       />
-
-      {/* Description TextArea (if needed) */}
-      {/* <textarea
-        placeholder='Add a description'
-        value={description}
-        onChange={handleChange}
-        name='description'
-        className='todo-textarea'
-      /> */}
-
-      <button onClick={handleSubmit} className='todo-button'>
+      <button type="submit" className='todo-button'>
         {edit ? 'Update todo' : 'Add todo'}
       </button>
     </form>
